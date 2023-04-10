ad712bfd73d02f0b9b3ca3f6c122008cc31e41f6
mmmtobezip <mmmszip@gmail.com>
2023-04-11 00:04:03 +0900

    <수정> Programmers_K번째수.java : Level 1

--- Programmers/ParkJihyeon/정렬/Programmers_K번째수.java
+++ Programmers/ParkJihyeon/정렬/Programmers_K번째수.java
@@ -19,7 +19,6 @@ public class Programmers_K번째수 {
     public static class Solution {
         public int[] solution(int[] array, int[][] commands) {
             int[] answer = new int[commands.length];
-//            int idx = 0;
 
             for (int i = 0; i < commands.length; i++) {
                 int sIndex = commands[i][0];
