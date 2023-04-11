3491ec2aff419c6954093255affc457c7b9fa425
mmmtobezip <mmmszip@gmail.com>
2023-04-11 23:19:39 +0900

    <수정> Programmers_hIndex.java : Level 2

--- Programmers/ParkJihyeon/정렬/Programmers_hIndex.java
+++ Programmers/ParkJihyeon/정렬/Programmers_hIndex.java
@@ -1,44 +1,29 @@
 import org.jetbrains.annotations.NotNull;
 
-public class Programmers_hIndex {
-    static int h;
-    static void insertSort(int @NotNull [] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int target = arr[i];
-            int j = i - 1;
+import java.util.Arrays;
 
-            while (j >= 0 && target < arr[j]) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = target;
-        }
-    }
+public class Programmers_hIndex {
     public static class Solution {
         public int solution(int[] citations) {
             int answer = 0;
+            int len = citations.length;
 
-            insertSort(citations);
+            Arrays.sort(citations);
 
-            h = citations[citations.length/2];
-
+            int i;
             int cnt = 0;
-            for (int i = 0; i < citations.length; i++) {
-                if (citations[i] >= h) {
-                    cnt++;
-                }
-            }
-            int low = 0;
-            int high = 0;
+            int index = len - 1;
 
-            while (low <= high) {
-                if (cnt >= h) {
-                    answer = h;
-                    low = h + 1;
-                } else if(h > cnt) {
-                    high = h - 1;
+            for (i = len; i >= 0; i--) {
+                while(citations[index] >= i){
+                    cnt ++;
+                    index --;
+//                    System.out.println("cnt: " + cnt+ " " +"index:" + index);
+                    if (index < 0) break;
                 }
+                if(cnt >= i) break;
             }
+            answer = i;
             return answer;
         }
 
