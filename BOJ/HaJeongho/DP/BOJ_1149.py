69ee0fce0a3b85ec2cd69a381b17818662fa9b15
hjh1248 <hjh4212@naver.com>
2023-08-02 18:08:44 +0900

    <추가> BOJ_1149.py: 실버

---
+++ BOJ/HaJeongho/DP/BOJ_1149.py
@@ -0,0 +1,23 @@
+n = int(input())
+def cost(n, memo=[[0,0], [0,1], [0,2]]):
+    if n == 0:
+        new_memo = []
+        for i in memo:
+            new_memo.append(i[0])
+        return print(min(new_memo))
+    tmp = []
+    array = input().split()
+    for j in memo:
+        if j[1] == 0:
+            tmp.append([j[0] + int(array[1]),1])
+            tmp.append([j[0] + int(array[2]),2])
+        if j[1] == 1:
+            tmp.append([j[0] + int(array[0]),0])
+            tmp.append([j[0] + int(array[2]),2])
+        if j[1] == 2:
+            tmp.append([j[0] + int(array[0]),0])
+            tmp.append([j[0] + int(array[1]),1])
+    memo = []
+    memo += tmp
+    return cost(n-1, memo)
+cost(n)
